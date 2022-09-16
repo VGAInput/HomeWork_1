1d59e43e36735fc514ab3ee73fb70d349ff1ebd3
Иван <ivanpaskhin@yandex.ru>
2022-09-16 23:01:17 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        String hello = new String("Hello Skypro!" + "\n:)");
+        String hello = new String("Hello, SkyPro!" + "\n:)");
 
         for (int i = 0; i < hello.length(); i++) {
             System.out.print(hello.charAt(i));
