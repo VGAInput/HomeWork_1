1b8ac33cb1420c1bc205abf18b5e95cae8607dfc
Иван <ivanpaskhin@yandex.ru>
2022-09-18 12:47:52 +0300

    lesson - 2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        String hello = new String("Hello, SkyPro!" + "\n:)");
+        String hello = new String("Hello, SkyPro!" + "\nlesson-2");
 
         for (int i = 0; i < hello.length(); i++) {
             System.out.print(hello.charAt(i));
